5e0ecc6a2960a2c9af34fc6ea94f08870cb6e8dd
on24hb <han.broska.24@lehre.mosbach.dhbw.de>
2026-01-23 13:42:00 +0100

    changed logo

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon-180.png', 'pwa-64x64.png', 'pwa-192x192.png', 'pwa-512x512.png', 'maskable-icon-512x512.png'],
       manifest: {
         name: 'Rezept App',
         short_name: 'Rezept',
@@ -19,21 +20,33 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
         {
-          src: 'manifest-icon-192.maskable.png',
+          src: 'pwa-64x64.png',
+          sizes: '64x64',
+          type: 'image/png',
+          purpose: 'any'
+        },
+        {
+          src: 'pwa-192x192.png',
           sizes: '192x192',
           type: 'image/png',
-          purpose: 'any maskable'
+          purpose: 'any'
         },
         {
-          src: 'manifest-icon-512.maskable.png',
+          src: 'pwa-512x512.png',
           sizes: '512x512',
           type: 'image/png',
-          purpose: 'any maskable'
+          purpose: 'any'
         },
         {
-          src: 'apple-icon-180.png',
+          src: 'apple-touch-icon-180.png',
           sizes: '180x180',
           type: 'image/png'
+        },
+        {
+          src: 'maskable-icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable'
         }
         ],
       },
